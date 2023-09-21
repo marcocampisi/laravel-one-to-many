8a8683006ee6262d910d2a17c8ac2c3076a3b423
marcocampisi <marco.campisi@outlook.it>
2023-09-21 14:39:58 +0200

    Complete type migrations

--- database/migrations/2023_09_21_123633_add_type_id_to_projects_table.php
+++ database/migrations/2023_09_21_123633_add_type_id_to_projects_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->foreignId('type_id')->nullable()->constrained();
         });
     }
 
@@ -22,7 +22,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropForeign('projects_type_id_foreign');
+            $table->dropColumn('type_id');
         });
     }
 };
